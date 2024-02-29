b066fd45765bdb91ad214cfd97a0949d73698667
Marco <marco.feliponi@outlook.com>
2024-02-29 20:41:56 -0300

    user model and routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const UserModel = require('../models/userModel');
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get('/', async (req, res) => {
+    try {
+        const users = await UserModel.find()
+        res.json(users)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.post('/', async (req, res) => {
+    const user = new UserModel({
+        bankAccountId: req.body.bankAccountId
+    })
+
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
 
 module.exports = router;
